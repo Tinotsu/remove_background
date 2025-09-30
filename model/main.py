85e724b393bd1c4e85badf25433e52533685aa0c
Tinotsu <tinopiof@gmail.com>
2025-09-30 16:12:41 +0200

    read image route

--- model/main.py
+++ model/main.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
 from pydantic import BaseModel
 from .rm_bg import remove_bg
+from fastapi.responses import FileResponse
 
 app = FastAPI()
 
@@ -28,3 +29,12 @@ class Image_URL(BaseModel):
 async def image_url(data: Image_URL):
     path = remove_bg(data.url)
     return {"saved": path}
+
+
+IMAGE_DIR = "/home/tino/Pictures/output_image3.png"
+
+
+@app.get("/img_wo_bg")
+async def read_image():
+    path = f"{IMAGE_DIR}"
+    return FileResponse(path)
